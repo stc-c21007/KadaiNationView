31692e5cc14e5dfabf4d91fbbb1539b4dc950b08
c21007 <c21007@suntech.jp>
2022-07-05 12:16:00 +0900

    Second Commit

--- app/src/main/java/jp/suntech/c21007/kadainationview/MainActivity.java
+++ app/src/main/java/jp/suntech/c21007/kadainationview/MainActivity.java
@@ -3,6 +3,13 @@ package jp.suntech.c21007.kadainationview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView ctlist = findViewById(R.id.countrylist);
+        ctlist.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            TextView list = findViewById(R.id.textView);    //持ってくる
+            TextView ctText = (TextView)view;
+            String item = ctText.getText().toString();
+
+            list.setText(item);
+        }
     }
 }
